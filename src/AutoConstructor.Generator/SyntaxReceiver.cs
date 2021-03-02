65723a7990bbe43dbe47edf49b106729b5fd8de2
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-03-02 20:35:43 +0100

    Add initial project

---
+++ src/AutoConstructor.Generator/SyntaxReceiver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AutoConstructor.Generator
+{
+    /// <summary>
+    /// Created on demand before each generation pass.
+    /// </summary>
+    internal class SyntaxReceiver : ISyntaxReceiver
+    {
+        public List<ClassDeclarationSyntax> CandidateClasses { get; } = new List<ClassDeclarationSyntax>();
+
+        /// <summary>
+        /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation.
+        /// </summary>
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            // Only check:
+            // - classes
+            // - with the "partial" keyword
+            // - with the wanted attribute
+            if (syntaxNode is ClassDeclarationSyntax classDeclarationSyntax
+                && classDeclarationSyntax.Modifiers.Any(SyntaxKind.PartialKeyword)
+                && classDeclarationSyntax.AttributeLists.Any(a =>
+                    a.Attributes.Any(b =>
+                        b.Name.ToString() == AutoConstructorGenerator.AttributeName || b.Name.ToString() == AutoConstructorGenerator.AttributeFullName)))
+            {
+                CandidateClasses.Add(classDeclarationSyntax);
+            }
+        }
+    }
+}
---
+++ src/AutoConstructor.Sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AutoConstructor.Sample
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var test = new Test2("test", DateTime.Now, Guid.NewGuid(), 89, "valm");
+            test.Dump();
+        }
+    }
+
+    [AutoConstructor]
+    internal partial class Test2
+    {
+        private readonly string _name;
+        private readonly Uri _uri = new("/non-modified", UriKind.Relative);
+
+        [AutoConstructorIgnore]
+        private readonly DateTime _date;
+
+        private readonly DateTime? _date2;
+
+        [AutoConstructorInject("guid.ToString()", "guid", typeof(Guid))]
+        private readonly string _fileName;
+
+        private readonly int _number;
+
+        [AutoConstructorInject("val.Length", "val", typeof(string))]
+        private readonly int _length;
+
+        public void Dump()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine(_uri);
+            Console.WriteLine(_date);
+            Console.WriteLine(_date2);
+            Console.WriteLine(_fileName);
+            Console.WriteLine(_number);
+            Console.WriteLine(_length);
+        }
+    }
+}
