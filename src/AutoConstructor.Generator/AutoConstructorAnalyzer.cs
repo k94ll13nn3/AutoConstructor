0fafcc1e96ec06035f7f1551a254b83971cf0dd6
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-07-08 14:31:09 +0200

    Add analyzer and diagnostic for attribute without partial

---
+++ src/AutoConstructor.Generator/AutoConstructorAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AutoConstructor.Generator
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AutoConstructorAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ACONS01";
+
+        private static readonly DiagnosticDescriptor Rule = new(
+            DiagnosticId,
+            "Couldn't generate consctructo",
+            $"Type decorated with {AutoConstructorGenerator.AttributeName} must be also declared partial",
+            "Usage",
+            DiagnosticSeverity.Error,
+            true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            _ = context ?? throw new ArgumentNullException(nameof(context));
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ClassDeclaration);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is ClassDeclarationSyntax classDeclarationSyntax
+                && classDeclarationSyntax.AttributeLists.Any(a =>
+                    a.Attributes.Any(b =>
+                        b.Name.ToString() == AutoConstructorGenerator.AttributeName || b.Name.ToString() == AutoConstructorGenerator.AttributeFullName))
+                && !classDeclarationSyntax.Modifiers.Any(SyntaxKind.PartialKeyword))
+            {
+                var diagnostic = Diagnostic.Create(Rule, classDeclarationSyntax.Identifier.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ src/AutoConstructor.Generator/AutoConstructorCodeFixProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace AutoConstructor.Generator
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AutoConstructorCodeFixProvider)), Shared]
+    public class AutoConstructorCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(AutoConstructorAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            SyntaxNode? root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            Diagnostic? diagnostic = context.Diagnostics[0];
+            TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            ClassDeclarationSyntax? declaration = root?.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
+            if (declaration is not null)
+            {
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title: "Make class partial",
+                        createChangedDocument: c => MakeClassPartialAsync(context.Document, declaration, c),
+                        equivalenceKey: "Make class partial"),
+                    diagnostic);
+            }
+        }
+
+        private static async Task<Document> MakeClassPartialAsync(Document document, ClassDeclarationSyntax classDeclarationSyntax, CancellationToken cancellationToken)
+        {
+            SyntaxTokenList newModifiers = classDeclarationSyntax.Modifiers.Add(SyntaxFactory.Token(SyntaxKind.PartialKeyword));
+            ClassDeclarationSyntax newDeclaration = classDeclarationSyntax.WithModifiers(newModifiers);
+
+            SyntaxNode? oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            if (oldRoot is not null)
+            {
+                SyntaxNode newRoot = oldRoot.ReplaceNode(classDeclarationSyntax, newDeclaration);
+                return document.WithSyntaxRoot(newRoot);
+            }
+
+            throw new InvalidOperationException("Cannot get syntax root.");
+        }
+    }
+}
