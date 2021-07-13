6ff8152011221523587182a284861aa06de5e89f
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-07-13 13:41:32 +0200

    Add tests for underscore trimming
    Also move analyzer/code fix tests to another file

---
+++ tests/AutoConstructor.Tests/AnalyzerAndCodeFixTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using VerifyClassWithoutFieldsToInject = AutoConstructor.Tests.Verifiers.CSharpCodeFixVerifier<
+    AutoConstructor.Generator.ClassWithoutFieldsToInjectAnalyzer,
+    AutoConstructor.Generator.ClassWithoutFieldsToInjectCodeFixProvider>;
+using VerifyClassWithoutPartial = AutoConstructor.Tests.Verifiers.CSharpCodeFixVerifier<
+    AutoConstructor.Generator.ClassWithoutPartialAnalyzer,
+    AutoConstructor.Generator.ClassWithoutPartialCodeFixProvider>;
+
+namespace AutoConstructor.Tests
+{
+    public class AnalyzerAndCodeFixTests
+    {
+        [Fact]
+        public async Task Analyzer_ClassWithoutPartial_ShouldReportDiagnostic()
+        {
+            const string test = @"
+namespace Test
+{
+    [AutoConstructor]
+    internal class {|#0:Test|}
+    {
+        private readonly int _t;
+    }
+}";
+
+            DiagnosticResult[] expected = new[] {
+                VerifyClassWithoutPartial.Diagnostic("ACONS01").WithLocation(0),
+            };
+            await VerifyClassWithoutPartial.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CodeFix_ClassWithoutPartial_ShouldFixCode()
+        {
+            const string test = @"
+namespace Test
+{
+    [AutoConstructor]
+    internal class {|#0:Test|}
+    {
+        private readonly int _t;
+    }
+}";
+
+            const string fixtest = @"
+namespace Test
+{
+    [AutoConstructor]
+    internal partial class Test
+    {
+        private readonly int _t;
+    }
+}";
+
+            DiagnosticResult[] expected = new[] {
+                VerifyClassWithoutPartial.Diagnostic("ACONS01").WithLocation(0),
+            };
+            await VerifyClassWithoutPartial.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+
+        [Fact]
+        public async Task Analyzer_ClassWithoutFieldsToInject_ShouldReportDiagnostic()
+        {
+            const string test = @"
+namespace Test
+{
+    [{|#0:AutoConstructor|}]
+    internal partial class Test
+    {
+    }
+}";
+
+            DiagnosticResult[] expected = new[] {
+                VerifyClassWithoutFieldsToInject.Diagnostic("ACONS02").WithLocation(0),
+            };
+            await VerifyClassWithoutFieldsToInject.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task Analyzer_ClassWithoutFieldsToInject_ShouldFixCode()
+        {
+            const string test = @"
+namespace Test
+{
+    class TT
+    {
+    }
+
+    [{|#0:AutoConstructor|}]
+    internal partial class Test
+    {
+    }
+}";
+            const string fixtest = @"
+namespace Test
+{
+    class TT
+    {
+    }
+
+    internal partial class Test
+    {
+    }
+}";
+
+            DiagnosticResult[] expected = new[] {
+                VerifyClassWithoutFieldsToInject.Diagnostic("ACONS02").WithLocation(0),
+            };
+            await VerifyClassWithoutFieldsToInject.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
