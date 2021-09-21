2f2c6905d1527bf6d886d30e98b0ba2f006f7d04
Kévin Gallienne <k94ll13nn3@outlook.fr>
2021-09-21 19:04:46 +0200

    Allow usage of AutoConstructorInject with some parameters not defined

--- src/AutoConstructor.Generator/FieldInfo.cs
+++ src/AutoConstructor.Generator/FieldInfo.cs
@@ -2,19 +2,22 @@
 
 public class FieldInfo
 {
-    public FieldInfo(string type, string parameterName, string fieldName, string initializer)
+    public FieldInfo(string? type, string parameterName, string fieldName, string initializer, string fallbackType)
     {
         Type = type;
         ParameterName = parameterName;
         FieldName = fieldName;
         Initializer = initializer;
+        FallbackType = fallbackType;
     }
 
-    public string Type { get; set; }
+    public string? Type { get; set; }
 
     public string ParameterName { get; set; }
 
     public string FieldName { get; set; }
 
     public string Initializer { get; set; }
+
+    public string FallbackType { get; set; }
 }
